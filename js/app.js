acc4a6bd2aef66eff30f68d983e524406b614e63
lenonbr <lenon.br@hotmail.com>
2025-03-27 14:57:46 -0300

    funções finalizadas

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 function alterarStatus(id) {
-    let status = ['Alugar', 'Devolver'];
-
     let jogoClicado = document.getElementById(`game-${id}`);
-
     let imagem = jogoClicado.querySelector('.dashboard__item__img');
     let botao = jogoClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = jogoClicado.querySelector('.dashboard__item__name');
 
-    alert(nomeJogo.textContent);
+    if(imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.textContent = "Alugar";
+        botao.classList.remove('dashboard__item__button--return')
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = "Devolver";
+        botao.classList.add('dashboard__item__button--return')
+    }
 }
